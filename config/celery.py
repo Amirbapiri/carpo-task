a47cb314ae119a229efd5043f0fe721662505d44
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 20:18:16 +0330

    activate celery scheduler

--- config/celery.py
+++ config/celery.py
@@ -1,8 +1,17 @@
 import os
 from celery import Celery
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.local')
+django.setup()
 
 celery = Celery('config')
 celery.config_from_object('django.conf:django', namespace='CELERY')
 celery.autodiscover_tasks()
+
+celery.conf.beat_schedule = {
+    'your-periodic-task': {
+        'task': 'monitoring.sensors.tasks.generate_sensor_data',
+        'schedule': 5.0,  # Run every 1 second
+    },
+}
