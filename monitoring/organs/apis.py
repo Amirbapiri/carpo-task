51811ec84b8ff32f19849fb35b5e7fb97bfd536b
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 15:39:19 +0330

    add permissions and authentication classes to the API

--- monitoring/organs/apis.py
+++ monitoring/organs/apis.py
@@ -3,14 +3,20 @@ from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from rest_framework import serializers, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Organ
+from .permissions import IsSysAdmin, IsHostAdmin
 
 
 User = get_user_model()
 
 
 class OrganAPI(APIView):
+    permission_classes = [IsAuthenticated, IsSysAdmin]
+    authentication_classes = [JWTAuthentication]
+    
     class OrganSysAdminOutputSerializer(serializers.ModelSerializer):
         class Meta:
             model = User
