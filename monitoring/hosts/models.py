f67665421d04112b467ff2e3f4f2b27591e5906a
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 18:02:55 +0330

    create models for hosts

--- monitoring/hosts/models.py
+++ monitoring/hosts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from monitoring.organs.models import Organ
+
+
 # Create your models here.
+class Host(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    organ = models.ForeignKey(
+        Organ,
+        on_delete=models.CASCADE,
+        related_name="hosts",
+    )
+
+    def __str__(self):
+        return f"{self.name} -> {self.organ}"
