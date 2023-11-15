cf577cc8c580c67062f4e8730390ead93666ee13
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 19:07:03 +0330

    add 'host_admin' field to Host model

--- monitoring/hosts/models.py
+++ monitoring/hosts/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 from monitoring.organs.models import Organ
 
 
-# Create your models here.
+User = get_user_model()
+
 class Host(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     organ = models.ForeignKey(
         Organ,
+        on_delete=models.CASCADE,
+        related_name="hosts",
+    )
+    host_admin = models.ForeignKey(
+        User,
         on_delete=models.CASCADE,
         related_name="hosts",
     )
