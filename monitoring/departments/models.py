591fdd0f3ed250b9e46a583483381d38c046eca2
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 18:27:39 +0330

    create Department model for 'departments'

--- monitoring/departments/models.py
+++ monitoring/departments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from monitoring.hosts.models import Host
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=120)
+    host = models.ForeignKey(
+        Host,
+        on_delete=models.CASCADE,
+        related_name="departments",
+    )
+
+    def __str__(self):
+        return self.name
