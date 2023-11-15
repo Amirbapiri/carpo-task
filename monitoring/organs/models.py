acbbc341a47ff105305546a7fff4be54613e5c22
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 15:31:32 +0330

    create 'Organ' model

--- monitoring/organs/models.py
+++ monitoring/organs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+# User with role of SYSADMIN can create instance of the following model
+
+
+class Organ(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    sys_admin = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="organs",
+    )
+
+    def __str__(self):
+        return self.name
