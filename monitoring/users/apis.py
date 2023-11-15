7a0ca612e109923204e53f0639d835c4fdb5f07d
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 14:37:27 +0330

    create input data serializer for system admin user registration

--- monitoring/users/apis.py
+++ monitoring/users/apis.py
@@ -2,6 +2,28 @@ from rest_framework.views import APIView
 from rest_framework import serializers
 
 
+from .models import SYSAdminUser
+
 
 class SysAdminRegistrationAPI(APIView):
-    pass
+    class InputSysAdminSerializer(serializers.Serializer):
+        email = serializers.EmailField(max_length=255)
+        password = serializers.CharField(max_length=100,)
+        confirm_password = serializers.CharField(max_length=100)
+
+        def validate_email(self, email):
+            if SYSAdminUser.objects.filter(email=email).exists():
+                raise serializers.ValidationError("Email's already taken")
+            return email
+
+        def validate(self, data):
+            if not data.get("password") or not data.get("confirm_password"):
+                raise serializers.ValidationError(
+                    "Please fill password and confirm password"
+                )
+
+            if data.get("password") != data.get("confirm_password"):
+                raise serializers.ValidationError(
+                    "confirm password is not equal to password"
+                )
+            return data
