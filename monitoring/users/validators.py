3f33c5216336c60d411d7559259a254a944e9d29
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 14:42:58 +0330

    create validator to check if password contains special char.

--- monitoring/users/validators.py
+++ monitoring/users/validators.py
@@ -20,3 +20,12 @@ def letter_validator(password):
             _("password must include letter"),
             code="password_must_include_letter",
         )
+
+
+def special_char_validator(password):
+    regex = re.compile("[@_!#$%^&*()<>?/\|}{~:]")
+    if regex.search(password) == None:
+        raise ValidationError(
+            _("password must include special char"),
+            code="password_must_include_special_char",
+        )
