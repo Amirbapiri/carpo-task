a031d7e93c4713fb8cd0e8eb85fcab2a5e52ef5a
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 15:35:50 +0330

    create POST for organs functionality

--- monitoring/organs/apis.py
+++ monitoring/organs/apis.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework.views import APIView
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.response import Response
 
 from .models import Organ
-
 
 
 User = get_user_model()
@@ -33,3 +33,19 @@ class OrganAPI(APIView):
 
         def get_sys_admin(self, organ_instance):
             return organ_instance.sys_admin.email
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.OrganInputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            organ = serializer.save(sys_admin=request.user)
+        except Exception as ex:
+            return Response(
+                f"Database error: {ex}",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        json_to_return = self.OrganOutputSerializer(organ).data
+        return Response(
+            json_to_return,
+            status=status.HTTP_201_CREATED,
+        )
