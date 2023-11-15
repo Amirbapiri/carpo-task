297136e076717baafd08954de3cbf33006cbb10e
Amir Bapiri <amirbapiri@gmail.com>
2023-11-15 15:31:50 +0330

    add Organ model to the admin site

--- monitoring/organs/admin.py
+++ monitoring/organs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Organ
 
-# Register your models here.
+
+@admin.register(Organ)
+class OrganAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "sys_admin")
+    search_fields = ("name", "sys_admin__email")
+    list_filter = ("sys_admin",)
